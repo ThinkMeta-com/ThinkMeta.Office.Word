f1fb447f9c58599cb1ca9494411b806d7d75b759
George <github@thinkmeta.com>
2025-04-17 09:27:18 +0200

    RTF-to-PDF conversion

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ThinkMeta.Office.Word
+
+Provides that use Microsoft Office Word Interop
+
+# Requirements
+
+Microsoft Word must be installed on the system.
+
+# Usage
+
+Currently, only RTF-to-PDF file conversion tool is available:
+
+```cs
+DocumentConverter.ConvertRtfToPdf("input.rtf", "output.pdf");
+```
---
+++ ThinkMeta.Office.Word/DocumentConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Office.Interop.Word;
+
+namespace ThinkMeta.Office.Word;
+
+/// <summary>
+/// Provides document conversion functions.
+/// </summary>
+public static class DocumentConverter
+{
+    /// <summary>
+    /// Converts an RTF file to PDF.
+    /// </summary>
+    /// <param name="rtfFilePath">The input path of the RTF file.</param>
+    /// <param name="pdfFilePath">The output path of the PDF file.</param>
+    public static void ConvertRtfToPdf(string rtfFilePath, string pdfFilePath) => ConvertFile(rtfFilePath, WdOpenFormat.wdOpenFormatRTF, pdfFilePath, WdSaveFormat.wdFormatPDF);
+
+    private static void ConvertFile(string inputFilePath, WdOpenFormat inputFormat, string outputFilePath, WdSaveFormat outputFormat)
+    {
+        var document = OpenWordApplication().Documents.Open(FileName: inputFilePath, Format: inputFormat);
+        document.SaveAs2(outputFilePath, outputFormat);
+        document.Close();
+    }
+
+    private static Application OpenWordApplication() => new() { Visible = false };
+}
